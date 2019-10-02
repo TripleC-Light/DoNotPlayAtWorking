8000a8b21abac8e1cbdd0788ca785c38f5743565
Edward Chou <triplec.light@gmail.com>
2019-10-02 12:17:54 +0800

    Create Webgame project

---
+++ static/Generate.js
@@ -0,0 +1,118 @@
+class Generate{
+	constructor(){
+		this.state = 'getID';
+		this.FSMtrigTime = 50;
+		this.ID = '';
+	}
+
+	pilot(parent, allObj){
+		switch(this.state){
+			case 'getID':
+				console.log('getID');
+				this.ID = '';
+				WS.send('getID');
+				this.state = 'checkID';
+				break;
+
+			case 'checkID':
+				console.log('checkID');
+				if(this.ID!=''){
+					if(idExist(myID, allObj)){
+						console.log('ID:' + this.ID + ' already exist');
+						this.state = 'finish';
+					}else{
+						console.log('ID:' + this.ID);
+						this.state = 'createPilot';
+					}
+				}
+				break;
+
+			case 'createPilot':
+				console.log('createPilot');
+				var _new = {
+					id : this.ID,
+					width: 70,
+					height: 70,
+					pic: ""
+				};
+				this.newObj(parent, _new);
+				var _mainMap = document.getElementById(parent);
+				WS.send('createPilot@' + _mainMap.getBoundingClientRect().left + ',' + _mainMap.getBoundingClientRect().top);
+				this.state = 'finish';
+				break;
+			case 'finish':
+				console.log('finish');
+				break;
+		}
+		if( this.state != 'finish' ){
+				var thisVar = this;
+				setTimeout(function(){
+					thisVar.pilot(parent, allObj);
+				}, this.FSMtrigTime);
+		}else{
+			this.state = 'getID';
+		}
+	}
+
+	newObj( parentObj, obj){
+		var _frameID = 'frame_' + obj.id;
+		this._createFrame(parentObj, obj.id);
+		this._createMsg(_frameID, obj.id);
+		this._createMsgArrow(_frameID, obj.id);
+		this._createMain(_frameID, obj.id);
+		this._createName(_frameID, obj.id);
+		this._createHp(_frameID, obj.id);
+		this._shapeMain(obj.id, obj.pic);
+	}
+
+	_createFrame( parentObj, id){
+		var _id = id;
+		var parent = document.getElementById(parentObj);
+		var pilotFrame = document.createElement("div");
+		pilotFrame.setAttribute("id", 'frame_' + _id);
+		pilotFrame.setAttribute("class", "cPilotFrame");
+		parent.appendChild(pilotFrame);
+		pilotFrame.style.visibility = 'hidden'
+	}
+	_createMsg( parentObj, id){
+		var parent = document.getElementById(parentObj);
+		var msg = document.createElement("div");
+		msg.setAttribute("id", 'msg_' + id);
+		msg.setAttribute("class", "cPilotmsg");
+		parent.appendChild(msg);
+	}
+	_createMsgArrow( parentObj, id){
+		var parent = document.getElementById(parentObj);
+		var msgArrow = document.createElement("div");
+		msgArrow.setAttribute("id", 'msgArrow_' + id);
+		msgArrow.setAttribute("class", "cPilotMsgArrow");
+		parent.appendChild(msgArrow);
+	}
+	_createName( parentObj, id){
+		var parent = document.getElementById(parentObj);
+		var name = document.createElement("div");
+		name.setAttribute("id", 'name_' + id);
+		name.setAttribute("class", "cPilotName");
+		name.innerHTML = id;
+		parent.appendChild(name);
+	}
+	_createHp( parentObj, id){
+		var parent = document.getElementById(parentObj);
+		var hp = document.createElement("div");
+		hp.setAttribute("id", 'HP_' + id);
+		hp.setAttribute("class", "cPilotHP");
+		parent.appendChild(hp);
+	}
+	_createMain( parentObj, id){
+		var parent = document.getElementById(parentObj);
+		var pilot = document.createElement("div");
+		pilot.setAttribute("id", id);
+		pilot.setAttribute("class", "cPilot");
+		parent.appendChild(pilot);
+	}
+	_shapeMain(id, pic){
+		var _main = new Object(id);
+		_main.reBorn();
+		allObj[allObj.length] = _main;
+	}
+}
---
+++ static/common.js
@@ -0,0 +1,61 @@
+
+class Common{
+	constructor(){
+		this.oldSysTimeForMsg = 0;
+		this.showMsgInMilliSecond = 0;
+		this.oldSysTimeForFPS = 0;
+		this.FPScount = 0;
+		this.gameFPS = 0;
+		this.tmpFPS = 0;
+	}
+
+	timeToUpdateMsg(){
+		var _sysTime = new Date().getTime();
+		if( (_sysTime-this.oldSysTimeForMsg)>this.showMsgInMilliSecond ){
+			this.oldSysTimeForMsg = _sysTime;
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	countFPS(){
+		if( this.FPScount>=10 ){
+			this.FPScount = 0;
+			this.gameFPS /= 10;
+			this.tmpFPS = this.gameFPS;
+			this.gameFPS = 0;
+		}else{
+			var _sysTime = new Date().getTime();
+			var _FPS = 1000 / (_sysTime - this.oldSysTimeForFPS);
+			if( _FPS>20 ){
+				// console.log('Data read from Buffer');
+			}else{
+    			this.oldSysTimeForFPS = _sysTime;
+				this.gameFPS += parseFloat(_FPS.toFixed(2));
+				this.FPScount += 1;
+			}
+		}
+		return this.tmpFPS.toFixed(2);
+	}
+
+	getXYinMap(_map){
+		var _mainMapBounding = $('#iMainMap')[0].getBoundingClientRect();
+		var e = window.event;
+		var x = e.clientX;
+		var y = e.clientY;
+		x = x-_mainMapBounding.left;
+		y = y-_mainMapBounding.top;
+		return ([Math.round(x),Math.round(y)]);
+	}
+}
+
+
+function idExist(id, objList){
+	for(var i=0; i<objList.length; i++){
+		if(id==objList[i].id){
+			return true;
+		}
+	}
+	return false;
+}
